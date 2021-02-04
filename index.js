72c235db4ec88a7a608eaea5397864f3723017b0
roe <roe@alteos.com>
2021-02-04 21:16:30 +0100

    Fields click

---
+++ index.js
@@ -0,0 +1,25 @@
+// let last = null;
+//
+// const field = [];
+//
+// function setField(fieldPosition){
+//     console.log(fieldPosition)
+// }
+
+// window.onload = (function() {
+//     // init();
+//     console.log('sdsddssd')
+// })();
+// console.log(document.readyState);
+
+// function work() {
+//     console.log('sdsdsdds')
+// }
+
+// if (document.readyState == 'loading') {
+//     // ещё загружается, ждём события
+//     document.addEventListener('DOMContentLoaded', work);
+// } else {
+//     // DOM готов!
+//     work();
+// }
